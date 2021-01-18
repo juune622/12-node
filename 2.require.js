dd065b3e8ace89e49d4d0336881db85f74abf696
juune622 <juune622@naver.com>
2021-01-19 08:00:08 +0900

    ..

--- 2.require.js
+++ 2.require.js
@@ -2,6 +2,8 @@ const {sum} = require('./02.util');
 const http = require('http');
 const os = require('os');
 const fs = require('fs');
+const moment = require('moment')
+const _ = require('lodash')
 
 const server =  http.createServer ((req,res)=>{
   res.setHeader('Content-type','text/html');
@@ -10,6 +12,10 @@ const server =  http.createServer ((req,res)=>{
 
 //console.log(os.cpus());
 //console.log(os.freemem());
-console.log(fs.readFileSync('./1.hello.js').toString());
+//console.log(fs.readFileSync('./1.hello.js').toString());
+//console.log(moment().format('YYYY-MM-DD hh:mm:ss'));
+const arr =[1,2,5,2,5,11,9];
+console.log(_.sortBy(arr))
+console.log(_.shuffle(arr))
 
-server.listen(1000,'127.0.0.1');
+server.listen(1000,'127.0.0.1'); 
